b6caecf68a696f1bcd2775b2f7c655164e2d30a5
bob-nx <me@nagexiucai.com>
2017-11-06 16:47:43 +0800

    add session into account

---
+++ account/session.php
@@ -0,0 +1,28 @@
+<?php
+// https://github.com/nagexiucai/website
+
+session_start();
+
+$_SESSION["username"] = 9527;
+
+foreach ($_SESSION as $key=>$value) {
+    echo $key . "=>" . $value . "<br/>";
+}
+
+foreach ($_COOKIE as $key=>$value) {
+    echo $key . "=>" . $value . "<br/>";
+}
+
+foreach ($_GET as $key=>$value) {
+    echo $key . "=>" .$value . "<br/>";
+}
+
+if (isset($_GET["password"])) {
+    echo "password hashed by default is " . password_hash($_GET["password"], PASSWORD_DEFAULT) . "<br/>";
+    echo "password hashed by bcrypt is " . password_hash($_GET["password"], PASSWORD_BCRYPT) . "<br/>";
+}
+
+echo password_hash("nagexiucai.com", PASSWORD_DEFAULT) . "<br/>";
+echo password_hash("nagexiucai.com", PASSWORD_BCRYPT) . "<br/>";
+
+password_verify("nagexiucai.com", "$2y$10$fjX.v43XmlP1OpS5r3C0/usTWedIkLS0PF3CG3WCNf/TttpJZhaui");
